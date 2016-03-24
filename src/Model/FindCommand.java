b405417f76fc54b4868988e16515dd9e1d6b7cfc
L <vasilegroza3@gmail.com>
2016-03-24 19:38:46 +0200

    some test FindCommand

--- src/Model/FindCommand.java
+++ src/Model/FindCommand.java
@@ -1,11 +1,15 @@
 package Model;
 
+import java.awt.PrintJob;
 import java.io.IOException;
 import java.nio.file.DirectoryIteratorException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
+
+import thredds.filesystem.ControllerOS7.PrintFiles;
 
 public class FindCommand implements Command {
 
@@ -23,12 +27,54 @@ public class FindCommand implements Command {
 		// TODO Auto-generated method stub
 		System.out.println("Command find execute...");
 		
+		try {
+			Files.walk(Paths.get(CdCommand.getInstance().getCurrentPath().toString())).forEach(filePath -> {
+			    if (Files.isReadable(filePath)) {
+			        System.out.println(filePath);
+			    }
+			});
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.err.println(e);
+		}
+		/*
+		String revertPath=CdCommand.getInstance().getCurrentPath().toString();
+		
+		Path dir=Paths.get(CdCommand.getInstance().getCurrentPath().toString());
+		if(parametres==null)
+		{
+			 System.out.println("give a search key");
+		}
+		else
+		{
+			
+			
+			try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+				
+				for (Path file: stream) {
+					//System.out.println(file.getFileName());
+					if( Pattern.matches("(.)+.wav",file.getFileName().toString()) || Pattern.matches("(.)+.mp3", file.getFileName().toString()) || Pattern.matches("(.)+.flac",file.getFileName().toString()) ){
+							 System.out.println(file.getFileName());
+					}
+				}
+				
+			   
+			} catch (IOException | DirectoryIteratorException x) {
+			    // IOException can never be thrown by the iteration.
+			    // In this snippet, it can only be thrown by newDirectoryStream.
+			    System.err.println(x);
+			}
+		}
+		*/
+	
+		
 	}
 	
 	private FindCommand()
 	{
 		
 	}
+	
 	
 
 }
